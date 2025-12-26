8584811b355cb5f1cb819d3e53e43f45aabf4d9d
KNK22 <148570046+KNK22@users.noreply.github.com>
2025-12-26 16:39:12 +0530

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "krishna-mart-v1";
+const CACHE_NAME = "krishna-mart-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -33,3 +33,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
